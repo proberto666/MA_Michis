182c90c86d31f3694834266cab7a45002e8764fe
proberto666 <maximilianohuerta38@gmail.com>
2021-09-27 16:58:10 -0500

    Cambios en pom.xml y en Servlet. Separación de funciones y corrección de contador de numeros primos

--- practicaMenu/src/main/java/web/Servlet.java
+++ practicaMenu/src/main/java/web/Servlet.java
@@ -2,9 +2,10 @@ package web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.*;
-import jdk.nashorn.internal.parser.TokenType;
 
 @WebServlet("/Servlet")
 public class Servlet extends HttpServlet{
@@ -13,57 +14,46 @@ public class Servlet extends HttpServlet{
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
         response.setContentType("text/html; charset=UTF-8");
         PrintWriter out= response.getWriter();
+        String endTags = "</body></html>";
 
-        String action = request.getParameter("opcion");
+        int action = Integer.parseInt(request.getParameter("opcion"));
         
+        out.print("<html>");
+        out.print("<head>");
+        out.print("<title>Resultado</title>");
+        out.print("<meta charset=\"UTF-8\"/>");
+        out.print("</head>");
+        out.print("<body>");
+
         switch(action){
-            case "1": 
+            case 1: 
                 double a = Double.parseDouble(request.getParameter("formulaGA"));
                 double b = Double.parseDouble(request.getParameter("formulaGB"));
                 double c = Double.parseDouble(request.getParameter("formulaGC"));
-                
-                double resultado1 = (b*(-1)+ Math.sqrt(b*b -4*(a*c)))/(2*a);
-                double resultado2 = (b*(-1)- Math.sqrt(b*b -4*(a*c)))/(2*a);
-                
-                
-                out.print("<html>");
-                out.print("<head>");
-                out.print("<title>Resultado</title>");
-                out.print("<meta charset=\"UTF-8\"/>");
-                out.print("</head>");
-                out.print("<body>");
+
+                ArrayList<Double> resultados = formulaGeneral(a, b, c); 
+               
                 out.print("<h1>Fórmula General</h1>");
                 out.print("<h2> a = "+ a + "</h2>");
                 out.print("<h2> b = "+ b + "</h2>");
                 out.print("<h2> c = "+ c + "</h2>");
                 out.print("<br>");
-                out.print("<h2> resultado 1 = "+ resultado1 + "</h2>");
-                out.print("<h2> resultado 2 = "+ resultado2 + "</h2>");
-                out.print("</body>");
-                out.print("</html>");
+                out.print("<h2> resultado 1 = "+ resultados.get(0) + "</h2>");
+                out.print("<h2> resultado 2 = "+ resultados.get(1) + "</h2>");
+                out.print(endTags);
             break;
-            case "2": 
-                
+            case 2: 
                 int n =  Integer.parseInt(request.getParameter("numero"));
-               
-                out.print("<html>");
-                out.print("<head>");
-                out.print("<title>Resultado</title>");
-                out.print("<meta charset=\"UTF-8\"/>");
-                out.print("</head>");
-                out.print("<body>");
                 out.print("<h1>Números primos</h1>");
+                ArrayList<Integer> nPrimos = primos(n);
                 
-                for(int i=2; i<= n; i++){
-                    if( i % n != 0){
-                        out.print("<h2>"+i+"</h2>");
-                    }
+                for (Integer integer : nPrimos) {
+                    out.print(integer + "</br>");
                 }
-                out.print("</body>");
-                out.print("</html>");
-            break;
-            case "3": 
-                
+
+                out.print(endTags);
+                break;
+            case 3: 
                 double ladoa = Double.parseDouble(request.getParameter("ladoa"));
                 double ladob = Double.parseDouble(request.getParameter("ladob"));
                 double ladoc = Double.parseDouble(request.getParameter("ladoc"));
@@ -79,23 +69,47 @@ public class Servlet extends HttpServlet{
                         tipoTriangulo = "Escaleno";
                     }
                 }
-                
-                out.print("<html>");
-                out.print("<head>");
-                out.print("<title>Resultado</title>");
-                out.print("<meta charset=\"UTF-8\"/>");
-                out.print("</head>");
-                out.print("<body>");
                 out.print("<h1>Tipo de Triángulo/h1>");
                 out.print("<h2>El triángulo es: "+tipoTriangulo+"</h2>");
-                out.print("</body>");
-                out.print("</html>");
-            break;
+                out.print(endTags);
+                break;
+            default:
+                break;
         }
         
-        
-        
-     
+    }
 
+    ArrayList<Double> formulaGeneral(double a, double b, double c){
+
+        ArrayList<Double> resultados = new ArrayList<>();
+        
+                
+        double resultado1 = (b*(-1)+ Math.sqrt(b*b -4*(a*c)))/(2*a);
+        double resultado2 = (b*(-1)- Math.sqrt(b*b -4*(a*c)))/(2*a);
+
+        resultados.add(resultado1);
+        resultados.add(resultado2);
+    
+        return resultados;
+    }
+
+    ArrayList<Integer> primos(int total){
+        ArrayList<Integer> numeros = new ArrayList<>();
+        int numero = 2;
+        int loop = 0;
+        while(loop<total){
+            int contador = 0;
+            for (int i = (int) Math.sqrt(numero); i > 1; i--) {
+                if (numero % i == 0) {
+                    contador++;
+                }
+            }
+            if(contador<1){
+                numeros.add(numero);
+                loop++;
+            }
+            numero ++;
+        }
+        return numeros;
     }
 }

1300854ed056d4df9aff1185b48fb20f8d7d558e
proberto666 <maximilianohuerta38@gmail.com>
2021-10-08 13:59:28 -0500

    cambios servlet

--- practicaJsp/src/main/java/web/DatosServlet.java
+++ practicaJsp/src/main/java/web/DatosServlet.java
@@ -1,15 +1,39 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package web;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
 
-/**
- *
- * @author YareliR
- */
-public class DatosServlet {
+@WebServlet("/DatosServlet")
+public class DatosServlet extends HttpServlet{
 
+     @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+
+        String nombre = request.getParameter("nombre");
+        String direccion = request.getParameter("direccion");
+        String telefono = request.getParameter("telefono");
+        String tecnologias = request.getParameter("tecnologias");
+        String experiencia = request.getParameter("experiencia");
+        String estadoCivil = request.getParameter("estadoCivil");
+
+        out.print("<html>");
+        out.print("<head>");
+        out.print("<title>DATOS PROCESADOS</title>");
+        out.print("<meta charset='UTF-8'>");
+        out.print("</head>");
+        out.print("<body><br>");
+        out.print("<div> <h1>Datos:</h1>");
+        out.print("<h2>Nombre: "+nombre+"</h2>");
+        out.print("<h2>Dirección: "+direccion+"</h2>");
+        out.print("<h2>Teléfono: "+telefono+"</h2>");
+        out.print("<h2>Tecnologías: "+tecnologias+"</h2>");
+        out.print("<h2>Experiencia: "+experiencia+"</h2>");
+        out.print("<h2>Estado Civil: "+estadoCivil+"</h2>");
+        out.print("</div>");
+        out.print("</body>");
+        out.print("</html>");
+    }
 }

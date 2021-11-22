549c9bee1336508e3711071abed34b0dc04dfdf6
Víctor Manuel Nevárez Herrera <victor.m.nevarez.h@gmail.com>
2021-11-22 15:10:55 -0600

    Clases en paquete dominio

--- Basura/Basura/src/main/java/dominio/Producto.java
+++ Basura/Basura/src/main/java/dominio/Producto.java
@@ -1,5 +1,56 @@
 package dominio;
 
 public class Producto {
+    private int idProducto;
+    private String nombre;
+    private int cantidad;
+    private double precio;
+
+    public Producto() {
+    }
+
+    public Producto(int idProducto, String nombre, int cantidad, double precio) {
+        this.idProducto = idProducto;
+        this.nombre = nombre;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+
+    public int getIdProducto() {
+        return idProducto;
+    }
+
+    public void setIdProducto(int idProducto) {
+        this.idProducto = idProducto;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Producto{" + "idProducto=" + idProducto + ", nombre=" + nombre + ", cantidad=" + cantidad + ", precio=" + precio + '}';
+    }
     
 }
